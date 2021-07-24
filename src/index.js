c58d1d05b697dcfeee1f9faf96b76e2293912799
Rolandas Dovydenas <rdovydenas@protonmail.ch>
2021-07-24 17:41:46 +0300

    added limit to express.json

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
 const authRoutes = require("./routes/auth.js");
